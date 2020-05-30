cdf67713e989c6ca09da6da3fd2fce250e67279a
Karol Krawczyk <42814139+Arencius@users.noreply.github.com>
2020-05-31 01:19:16 +0200

    removed unused import

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import ElementNotVisibleException
 from selenium.webdriver.common.action_chains import ActionChains
 from secrets import GOOGLE_EMAIL, PASSWORD
 from time import sleep
